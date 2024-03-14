9f589c045bb9c9018500b71593cd85194206f071
Pasupuleti Arun <arunkumar.pasupuleti774@gmail.com>
2024-03-14 18:43:14 +0530

    Todo App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css";
+import React, { useState, useEffect } from "react";
+import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
+import Notification from "./components/Notification";
+import FilterTodos from "./components/FilterTodos";
+import { Button } from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [showForm, setShowForm] = useState(false);
+    const [editable, setEditable] = useState(false);
+    const [todoData, setTodoData] = useState([]);
+    const [clickedTodo, setClickedTodo] = useState(null);
+    const [notification, setNotification] = useState(null);
+    const [sortedTodos, setSortedTodos] = useState([]);
+    const fetchData = () => {
+        const data = [];
+        for (let i = 0; i < localStorage.length; i++) {
+            let key = localStorage.key(i);
+            let value = localStorage.getItem(key);
+            value = JSON.parse(value);
+            data.push(value);
+        }
+        setTodoData(data);
+        setSortedTodos(data);
+    };
+    useEffect(() => {
+        fetchData();
+    }, []);
+    const handleClick = () => {
+        setShowForm((val) => !val);
+    };
+    const handleAddTodo = (todo) => {
+        setTodoData((prevTodo) => [...prevTodo, todo]);
+    };
+    return (
+        <div className="todo-list-app">
+            <div className="header">
+                <h1>Todo Application</h1>
+            </div>
+            {notification && <Notification notification={notification} />}
+            <br />
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    marginBottom: "1rem",
+                }}
+            >
+                <Button
+                    onClick={handleClick}
+                    variant="contained"
+                    color="success"
+                >
+                    {editable ? "Update Todo" : "Add Todo"}
+                </Button>
+            </div>
+            {showForm && (
+                <AddTodoForm
+                    setShowForm={setShowForm}
+                    handleAddTodo={handleAddTodo}
+                    editable={editable}
+                    setEditable={setEditable}
+                    fetchData={fetchData}
+                    clickedTodo={clickedTodo}
+                    setClickedTodo={setClickedTodo}
+                    setNotification={setNotification}
+                />
+            )}
+            <h2 style={{ background: "#A460ED", color: "black" }}>
+                Your Todos
+            </h2>
+            <FilterTodos
+                todoData={todoData}
+                setSortedTodos={setSortedTodos}
+                setClickedTodo={setClickedTodo}
+            />
+            <hr style={{ height: ".2rem", background: "black" }} />
+            {localStorage.length === 0 ? (
+                <p>
+                    There are no Todos currently <br /> Add new Todos to view.
+                </p>
+            ) : (
+                <TodoList
+                    sortedTodos={sortedTodos}
+                    fetchData={fetchData}
+                    setEditable={setEditable}
+                    setShowForm={setShowForm}
+                    clickedTodo={clickedTodo}
+                    setClickedTodo={setClickedTodo}
+                    setNotification={setNotification}
+                />
+            )}
+        </div>
+    );
 }
 
 export default App;
